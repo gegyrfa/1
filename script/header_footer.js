112f0e33db0e51570cc4231b996592d4b7f92ece
gegyrfa <yalevknyazev@gmail.com>
2024-11-23 12:37:47 +0300

    Update header_footer.js

--- script/header_footer.js
+++ script/header_footer.js
@@ -1,8 +1,8 @@
 document.addEventListener("DOMContentLoaded", function () {
-fetch(`files/header.html`)
-.then(response => response.text())
-.then(data => document.querySelector(`header`).innerHTML = data);
-fetch("files/footer.html")
-.then(response => response.text())
-.then(data => document.querySelector("footer").innerHTML = data);
+  fetch(`files/header.html`)
+  .then(response => response.text())
+  .then(data => document.querySelector(`header`).innerHTML = data);
+  fetch("files/footer.html")
+  .then(response => response.text())
+  .then(data => document.querySelector("footer").innerHTML = data);
 });
